55f4bb3b0d80ce6feb9d9aece72b175c3c8e2553
GC1A-TinLong <147121460+GC1A-TinLong@users.noreply.github.com>
2024-04-19 00:34:09 +0900

    00_04_課題

--- main.cpp
+++ main.cpp
@@ -9,7 +9,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
-	
+	Vector3 rotate{ 0.4f,1.43f,-0.8f };
+	Matrix4x4 rotateMatrix_X = MakeRotateXMatrix(rotate.x);
+	Matrix4x4 rotateMatrix_Y = MakeRotateYMatrix(rotate.y);
+	Matrix4x4 rotateMatrix_Z = MakeRotateZMatrix(rotate.z);
+	Matrix4x4 rotateMatrix_XYZ = rotateMatrix_X * rotateMatrix_Y * rotateMatrix_Z;
 
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
@@ -36,7 +40,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-		
+		MatrixScreenPrintf(0, 0, rotateMatrix_X, "RotateMatrix_X");
+		MatrixScreenPrintf(0, kRowHeight * 5, rotateMatrix_Y, "RotateMatrix_Y");
+		MatrixScreenPrintf(0, kRowHeight * 5 * 2, rotateMatrix_Z, "RotateMatrix_Z");
+		MatrixScreenPrintf(0, kRowHeight * 5 * 3, rotateMatrix_XYZ, "RotateMatrix_XYZ");
 
 		///
 		/// ↑描画処理ここまで
